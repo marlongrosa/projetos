c947404e994378027d15b743eed158c15969e2a1
Marlon Rosa <114532602+marlongrosa@users.noreply.github.com>
2023-12-12 07:11:02 -0300

    Update atualizatotem.sh

--- atualizatotem.sh
+++ atualizatotem.sh
@@ -1,4 +1,4 @@
-# sudo wget --inet4-only -O- https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/UpdateServicesBeta-2023.sh | bash
+# sudo wget --inet4-only -O- https://raw.githubusercontent.com/marlongrosa/projetos/main/atualizatotem.sh | bash
 log() 
 {
     echo $(date)
