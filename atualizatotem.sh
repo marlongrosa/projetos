5a0e6b700c5e4b72094144f231171ebb95b6ca70
Marlon Rosa <114532602+marlongrosa@users.noreply.github.com>
2024-08-20 17:07:37 -0300

    atualizatotem.sh

--- atualizatotem.sh
+++ atualizatotem.sh
@@ -9,13 +9,13 @@ log()
 
 # Versions
 VsOsInterface="2.23.5"
-VsAutoPagSE="2.29.0"
+VsAutoPagSE="2.31.2"
 #VsPrint="2.18.0"
 VsFoodLauncher="2.0.0"
 # Output
 killall chrome
-wget --inet4-only -c https://images.food.vsd.app/uploads/10075/advertising/2024/05/09/8645317ab77b45f7.gif
-google-chrome --password-store=basic --kiosk --disable-pinch 8645317ab77b45f7.gif &>/dev/null &
+#wget --inet4-only -c https://images.food.vsd.app/uploads/10075/advertising/2024/05/09/8645317ab77b45f7.gif
+#google-chrome --password-store=basic --kiosk --disable-pinch 8645317ab77b45f7.gif &>/dev/null &
 
 # Prepare
 sudo rm /var/lib/dpkg/lock-frontend
