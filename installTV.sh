4b5c53663a06a27783a49cfd79871768a2bb6d47
Marlon Rosa <114532602+marlongrosa@users.noreply.github.com>
2024-09-02 16:43:29 -0300

    Update installTV.sh

--- installTV.sh
+++ installTV.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # sudo wget -O- https://raw.githubusercontent.com/marlongrosa/projetos/main/installTV.sh | bash
 
-#!/bin/bash
-
 # Parar o serviço do TeamViewer
 sudo systemctl stop teamviewerd
+
+# Parar o serviço do AnyDesk
+sudo systemctl stop anydesk
 
 # Remover e reconfigurar o machine-id
 sudo rm -rf /etc/machine-id
 sudo systemd-machine-id-setup
+
+# Remover o ID do AnyDesk
+sudo rm -rf /var/lib/anydesk/service.conf
+sudo rm -rf /etc/anydesk/service.conf
 
 # Aceitar automaticamente os termos do TeamViewer
 sudo teamviewer --daemon stop
@@ -17,5 +22,11 @@ sudo teamviewer --accept-license
 # Configurar o TeamViewer para iniciar junto com o sistema operacional
 sudo systemctl enable teamviewerd
 
+# Configurar o AnyDesk para iniciar junto com o sistema operacional
+sudo systemctl enable anydesk
+
 # Iniciar o serviço do TeamViewer
 sudo systemctl start teamviewerd
+
+# Iniciar o serviço do AnyDesk
+sudo systemctl start anydesk
