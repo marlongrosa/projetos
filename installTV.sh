8b518c92fcced58b81376990a996471e58d526e4
Marlon Rosa <114532602+marlongrosa@users.noreply.github.com>
2024-09-02 13:29:39 -0300

    Update installTV.sh

--- installTV.sh
+++ installTV.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # sudo wget -O- https://raw.githubusercontent.com/marlongrosa/projetos/main/installTV.sh | bash
 
+# Parar o serviço do TeamViewer
 systemctl stop teamviewerd
+
+# Remover e reconfigurar o machine-id
 sudo rm -rf /etc/machine-id && sudo systemd-machine-id-setup
+
+# Aceitar automaticamente os termos do TeamViewer
+teamviewer setup --accept-license
+
+# Configurar o TeamViewer para iniciar junto com o sistema operacional
+teamviewer --daemon enable
+
+# Iniciar o serviço do TeamViewer
 teamviewer --daemon start
-exit
-teamviewer
